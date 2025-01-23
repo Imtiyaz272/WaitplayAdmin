da38fd24ecd153c871b03a117108eef1e30fe78a
Imtiyaz272 <syedaimtiyaz19@gmail.com>
2025-01-23 18:11:18 +0530

    Update Sidebar

--- frontend/src/components/SideBar/SideBar.js
+++ frontend/src/components/SideBar/SideBar.js
@@ -1,71 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBox, FaHome, FaUtensils, FaQrcode, FaReceipt, FaUserFriends, FaCog, FaBars, FaRegUserCircle } from "react-icons/fa";
 import { useNavigate } from "react-router-dom"; 
 import "./SideBar.css";
 import logowp from "../../images/logowp.png";
 
 const SideBar = () => {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
+  const [isCollapsed, setIsCollapsed] = useState(false); 
 
   const handleNavigation = (path) => {
     navigate(path); 
   };
 
-  // const handleSignOut = () => {
-  //   console.log("Sign-out button clicked");
-  // };
+  const toggleSidebar = () => {
+    setIsCollapsed(!isCollapsed); 
+  };
 
   return (
-    <div className="flex flex-col h-screen w-64 bg-gradient-to-b from-blue-800 to-purple-700 text-white">
-      <div className="flex justify-between items-center">
-        <div className="inline-block">
-        <img
-          src={logowp} 
-          className="w-36"
-          alt="WaitPlay Logo"
-        />
-        </div>
-        <div className="px-3">
+    <div
+      className={`flex flex-col h-screen ${
+        isCollapsed ? "w-20" : "w-64"
+      } bg-gradient-to-b from-blue-800 to-purple-700 text-white transition-all duration-300`}
+    >
+      <div className="flex justify-between items-center p-3">
+        {!isCollapsed && (
+          <img src={logowp} className="w-36" alt="WaitPlay Logo" />
+        )}
+        <button className="text-white" onClick={toggleSidebar}>
           <FaBars size={24} />
+        </button>
+      </div>
+
+        <div className="flex items-center space-x-2 justify-center mt-6">
+          <FaRegUserCircle size={36} />
+          {!isCollapsed && (
+          <span className="text-lg">Restaurant Name</span>
+        )}
         </div>
-      </div>
-      <div className="flex items-center space-x-2 justify-center mt-6">
-        <FaRegUserCircle size={36}/>
-        <span className="text-lg">Restaurant Name</span>
-      </div>
+
       <nav className="flex-1 md:mt-5">
         <ul className="space-y-1">
-          <li className="flex items-center px-10 py-2 cursor-pointer hover:bg-blue-500 text-xl" onClick={() => handleNavigation("/dashboard")}>
+          <li
+            className={`flex items-center ${!isCollapsed ? "px-10" : "px-6"} py-2 cursor-pointer hover:bg-blue-500 text-xl`}
+            onClick={() => handleNavigation("/dashboard")}
+          >
             <FaHome className="mr-3" />
-            <span>Business</span>
+            {!isCollapsed && <span>Business</span>}
           </li>
-          <li className="flex items-center px-10 py-2 cursor-pointer hover:bg-blue-500 text-xl" onClick={() => handleNavigation("/orders")}>
+          <li
+            className={`flex items-center ${!isCollapsed ? "px-10" : "px-6"} py-2 cursor-pointer hover:bg-blue-500 text-xl`}
+            onClick={() => handleNavigation("/orders")}
+          >
             <FaBox className="mr-3" />
-            <span>Orders</span>
+            {!isCollapsed && <span>Orders</span>}
           </li>
-          <li className="flex items-center px-10 py-2 cursor-pointer hover:bg-blue-500 text-xl" onClick={() => handleNavigation("/table")}>
+          <li
+            className={`flex items-center ${!isCollapsed ? "px-10" : "px-6"} py-2 cursor-pointer hover:bg-blue-500 text-xl`}
+            onClick={() => handleNavigation("/table")}
+          >
             <FaQrcode className="mr-3" />
-            <span>Tables</span>
+            {!isCollapsed && <span>Tables</span>}
           </li>
-          <li className="flex items-center px-10 py-2 cursor-pointer hover:bg-blue-500 text-xl">
+          <li className={`flex items-center ${!isCollapsed ? "px-10" : "px-6"} py-2 cursor-pointer hover:bg-blue-500 text-xl`}>
             <FaReceipt className="mr-3" />
-            <span>Bills</span>
+            {!isCollapsed && <span>Bills</span>}
           </li>
-          <li className="flex items-center px-10 py-2 cursor-pointer hover:bg-blue-500 text-xl">
+          <li className={`flex items-center ${!isCollapsed ? "px-10" : "px-6"} py-2 cursor-pointer hover:bg-blue-500 text-xl`}>
             <FaUserFriends className="mr-3" />
-            <span>Requests</span>
+            {!isCollapsed && <span>Requests</span>}
           </li>
-          <li className="flex items-center px-10 py-2 cursor-pointer hover:bg-blue-500 text-xl" onClick={() => handleNavigation("/menu")}>
+          <li
+            className={`flex items-center ${!isCollapsed ? "px-10" : "px-6"} py-2 cursor-pointer hover:bg-blue-500 text-xl`}
+            onClick={() => handleNavigation("/menu")}
+          >
             <FaUtensils className="mr-3" />
-            <span>Menu</span>
+            {!isCollapsed && <span>Menu</span>}
           </li>
         </ul>
       </nav>
 
+      {/* Settings */}
       <div className="mb-5">
-        <div className="flex items-center px-10 py-2 text-xl">
-        <FaCog className="mr-2" />
-        <span>Settings</span>
+        <div className={`flex items-center ${!isCollapsed ? "px-10" : "px-6"} py-2 text-xl`}>
+          <FaCog className="mr-2" />
+          {!isCollapsed && <span>Settings</span>}
         </div>
       </div>
     </div>
@@ -73,77 +91,3 @@ const SideBar = () => {
 };
 
 export default SideBar;
-
-
-// import React from "react";
-// import { FaBox, FaHome, FaUtensils,FaQrcode, FaReceipt, FaUserFriends } from "react-icons/fa";
-// import { useNavigate } from "react-router-dom"; 
-// import waitplayLogo from "../../images/waitplay_logo.jpg";
-// import devlogo from "../../images/devloper.png"
-// import "./SideBar.css";
-
-// const SideBar=() => {
-//   const navigate = useNavigate(); 
-
-//   const handleNavigation = (path) => {
-//     navigate(path); 
-//   };
-//   const handleSignOut = () => {
-//     console.log("Sign-out button clicked");
-//   };
-
-//   return (
-//     <div className="sidebar">
-//       <div className="logo-container">
-//         <img
-//           src={waitplayLogo} 
-//           className="logo-image"
-//           alt="WaitPlay Logo"
-//         />
-//       </div>
-//       <div className="bold-text">WaitPlay</div>
-//       <div className="restaurant-name">
-//         <h2 className="restaurant-name-text"> Restaurant Name </h2>
-//       </div>
-
-//       <ul className="menu">
-//         <li onClick={() => handleNavigation("/dashboard")}>
-//           <FaHome />
-//           <div className="menu-items"> Business </div>
-//         </li>
-//         <li onClick={() => handleNavigation("/orders")}>
-//           <FaBox />
-//           <div className="menu-items"> Orders </div>
-//         </li>
-//         <li onClick={() => handleNavigation("/table")}>
-//           <FaQrcode />
-//           <div className="menu-items"> Tables </div>
-//         </li>
-//         <li>
-//           <FaReceipt />
-//           <div className="menu-items"> Bills </div>
-//         </li>
-//         <li>
-//           <FaUserFriends />
-//           <div className="menu-items"> Requests </div>
-//         </li>
-//         <li onClick={() => handleNavigation("/menu")}>
-//           <FaUtensils />
-//           <div className="menu-items"> Menu </div>
-//         </li>
-//       </ul>
-//       <div className="sidebar-bottom">
-//         <img
-//           src={devlogo}
-//           alt="User Profile"
-//           className="sidebar-bottom-image"
-//         />
-//         <button className="signout-button" onClick={handleSignOut}>
-//           Sign Out
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default SideBar;
